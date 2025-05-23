c72e531fc4799d3e83ea5410b2cbe00648267452
Monixc <chris13404@gmail.com>
2025-05-24 07:26:44 +0900

    feat: JWT 인증 미들웨어 및 사용자 검증 미들웨어 공통으로 분리

--- backend/src/features/todos/todos.routes.ts
+++ backend/src/features/todos/todos.routes.ts
@@ -1,21 +1,21 @@
 import express from "express";
 import * as todosController from "./todos.controller";
-import authMiddleware from "../auth/auth.middleware";
+import jwtAuthMiddleware from "../../middlewares/jwt.middleware";
 import { isTeamMember, isTeamLeader } from "../teams/team-auth.middleware";
 
 const todosRouter = express.Router();
 
 
-todosRouter.post("/", authMiddleware, todosController.createPersonalTodo);
-todosRouter.get("/", authMiddleware, todosController.getPersonalTodos);
-todosRouter.patch("/:todoId", authMiddleware, todosController.updatePersonalTodo);
-todosRouter.delete("/:todoId", authMiddleware, todosController.deletePersonalTodo);
-todosRouter.patch("/:todoId/toggle", authMiddleware, todosController.togglePersonalTodoComplete);
+todosRouter.post("/", jwtAuthMiddleware, todosController.createPersonalTodo);
+todosRouter.get("/", jwtAuthMiddleware, todosController.getPersonalTodos);
+todosRouter.patch("/:todoId", jwtAuthMiddleware, todosController.updatePersonalTodo);
+todosRouter.delete("/:todoId", jwtAuthMiddleware, todosController.deletePersonalTodo);
+todosRouter.patch("/:todoId/toggle", jwtAuthMiddleware, todosController.togglePersonalTodoComplete);
 
-todosRouter.post("/team/:teamId", authMiddleware, isTeamMember, todosController.createTeamTodo);
-todosRouter.get("/team/:teamId", authMiddleware, isTeamMember, todosController.getTeamTodos);
-todosRouter.patch("/team/:teamId/:todoId", authMiddleware, isTeamMember, todosController.updateTeamTodo);
-todosRouter.delete("/team/:teamId/:todoId", authMiddleware, isTeamLeader, todosController.deleteTeamTodo);
-todosRouter.patch("/team/:teamId/:todoId/toggle", authMiddleware, isTeamMember, todosController.toggleTeamTodoComplete);
+todosRouter.post("/team/:teamId", jwtAuthMiddleware, isTeamMember, todosController.createTeamTodo);
+todosRouter.get("/team/:teamId", jwtAuthMiddleware, isTeamMember, todosController.getTeamTodos);
+todosRouter.patch("/team/:teamId/:todoId", jwtAuthMiddleware, isTeamMember, todosController.updateTeamTodo);
+todosRouter.delete("/team/:teamId/:todoId", jwtAuthMiddleware, isTeamLeader, todosController.deleteTeamTodo);
+todosRouter.patch("/team/:teamId/:todoId/toggle", jwtAuthMiddleware, isTeamMember, todosController.toggleTeamTodoComplete);
 
 export default todosRouter; 
